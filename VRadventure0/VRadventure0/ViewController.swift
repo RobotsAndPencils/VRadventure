9174dc6341fdad2cd46a899325ebef9a198677f1
tedgoddard <ted.goddard@robotsandpencils.com>
2016-08-04 13:42:29 -0600

    bare bones Xcode project

---
+++ VRadventure0/VRadventure0/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  VRadventure0
+//
+//  Created by Ted Goddard on 2016-08-04.
+//  Copyright © 2016 Robots & Pencils. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var leftView: SCNView!
+    @IBOutlet weak var rightView: SCNView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
