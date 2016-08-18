c392238392ae0d93f42361802d4b180edc235b23
tedgoddard <ted.goddard@robotsandpencils.com>
2016-08-18 13:37:14 -0700

    initial empty project

---
+++ VRadventure1/VRadventure1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VRadventure1
+//
+//  Created by Ted Goddard on 2016-08-18.
+//  Copyright © 2016 Robots & Pencils. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
